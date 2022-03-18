e4fbf0b2fc95bca735afef358cdf24315f90a1c2
Leonardorossato <leonardorossato21@gmail.com>
2022-03-18 15:38:50 -0300

    ajuste

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.port
 const mongoConnection = require('./connection/mongoConnection')
+const livrosRouter = require('./routes/livrosRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.mongoConnection = mongoConnection
+
+app.use('/api/livros', livrosRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is listening the port : ${port}`)
